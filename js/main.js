9082e364e10341f815bea942b564c8ee31fdce60
Davivan <dimm.software@gmail.com>
2021-04-21 22:57:26 -0500

    Se agrega logica de filto por fechas

--- js/main.js
+++ js/main.js
@@ -7,7 +7,32 @@
 
 /*Fin*/
 /*Seccion Ivan */
+$(document).ready(() => {
 
+  $(".btn").click(function () {
+    const value = $(this).attr("data-filter");
+    switch (value) {
+      case "feed":
+        console.log("Feed");
+        break;
+      case "week":
+        console.log("Week");
+        break;
+      case "month":
+        console.log("Month");
+        break;
+      case "year":
+        console.log("Year");
+        break;
+      case "infinity":
+        console.log("Inifnity");
+        break;
+      default:
+        console.log("Not used");
+    }
+  });
+
+});
 
 /*FIn*/
 /*Seccion Jav */
